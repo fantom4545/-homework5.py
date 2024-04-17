924fe1299deac826479f4544b379a3e5e080865f
fantom4545 <166008514+fantom4545@users.noreply.github.com>
2024-04-17 23:49:28 +0700

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,14 @@
+BioWdre=['Mass Effect','Mass Effect 2','Mass Effect 3','Dragon Age','Dragon Age 2','Dragon Age : Inqusition','Anthem']
+print(BioWdre)
+print(BioWdre[0])
+print(BioWdre[-1])
+print(BioWdre[3:6])
+BioWdre[3]='Mass Effect 5'
+print(BioWdre)
+my_dic={"geme":"игра","hempad":"гемпад","PC":"ПК","screen":"монитор"}
+print(my_dic)
+print(my_dic["PC"])
+print(my_dic.values())
+my_dic["PC"]="компьютер"
+print(my_dic)
+
